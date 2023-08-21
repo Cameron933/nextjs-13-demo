684e555438595b774ada084ea63cc2f6848ec859
Cameron Huang <73293255+Cameron933@users.noreply.github.com>
2023-08-21 23:44:16 +1000

    todo page paragraph & its style

--- app/todos/[todoId]/page.tsx
+++ app/todos/[todoId]/page.tsx
@@ -19,7 +19,17 @@ const fetchTodo = async (todoId: string) => {
 const TodoPage = async ({ params: { todoId } }: PageProps) => {
   const todo: Todo = await fetchTodo(todoId);
 
-  return <div>TodoPage: {todoId}</div>;
+  return (
+    <div className="p-10 bg-yellow-200 border-2 m-2 shadow-lg">
+      <p>
+        #{todo.id}: {todo.title}
+      </p>
+      <p>Completed: {todo.completed ? "Yes" : "No"}</p>
+      <p className="border-t border-black mt-5 text-right">
+        By User: {todo.userId}
+      </p>
+    </div>
+  );
 };
 
 export default TodoPage;
